517a12b41e996703094237a2fe0e64cc32dc1d01
Kairi Wright <kanwright19@gmail.com>
2024-01-18 01:06:36 -0500

    changed the scope

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+var customerName = "bob";
+//var bestCustomer = "not bob"
+const leastFavoriteCustomer = "james"
+
+    function upperCaseCustomerName(){
+        return customerName = customerName.toUpperCase();
+      
+    }
+
+    var bestCustomer = 'not bob'
+
+    function setBestCustomer(){
+        bestCustomer = 'amy'
+        return bestCustomer 
+    }
+
+    function overwriteBestCustomer(){
+        return bestCustomer = "maybe bob"
+    }
+    
+    function changeLeastFavoriteCustomer(){
+        return leastFavoriteCustomer = "marsha"
+    }
+
+//setBestCustomer(): Write a function that when called, 
+//declares a variable called bestCustomer in global scope 
+//and assigns it to be 'not bob'. (Poor Bob.) Also, poor us,
+// declaring a global variable from inside a function is one of those things we would never want to do normally,
+// but is good for us to explore right now.
